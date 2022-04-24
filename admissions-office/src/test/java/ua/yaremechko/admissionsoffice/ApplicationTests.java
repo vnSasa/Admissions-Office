fe7fad6864055a920ca9baefe2fad521fb399c23
vnSasa <ol.yaremechko@gmail.com>
2022-04-24 08:35:55 +0300

    init project

---
+++ admissions-office/src/test/java/ua/yaremechko/admissionsoffice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yaremechko.admissionsoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
